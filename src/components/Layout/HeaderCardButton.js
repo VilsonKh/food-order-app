bcd4e6c3c0192f709edc450becef8264399cde64
Vilson <khvilson@yandex.ru>
2023-04-26 12:11:40 +0300

    added animation to busket buttom

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,19 +1,39 @@
-import {useContext} from "react";
+import {useContext, useEffect,useState} from "react";
 import CartContext from "../../store/cart-context"
 import CardIcon from "..//Card/CardIcon";
 import styles from "./HeaderCardButton.module.css";
 
 
 const HeaderCardButton = (props) => {
+
+	const [isButtonAmimated, setIsButtonAnimated] = useState(false)
+
 	const cartContext = useContext(CartContext)
 
 	const cartItemsNumber = cartContext.items.reduce((currentValue, item) => {
 		return currentValue + item.amount
 	},0)
 
+	const buttonClasses = `${styles.button} ${isButtonAmimated ? styles.bump : ''}`
+
+	useEffect(() => {
+		if(cartContext.items.length === 0) {
+			return;
+		}
+
+		setIsButtonAnimated(true);
+
+		const timer = setTimeout(()=>{
+			setIsButtonAnimated(false)
+		},300)
+
+		return () => {
+			clearTimeout(timer)
+		}
+	}, [cartContext.items])
 
 	return (
-		<button className={styles.button} onClick={props.onClick}>
+		<button className={buttonClasses} onClick={props.onClick}>
 			<span className={styles.icon}>
         <CardIcon></CardIcon>
       </span>
